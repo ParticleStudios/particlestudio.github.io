36b8660f8eaae8d6cccb8c28cfe23e2cdbaabf3a
abused_master <techperson71@gmail.com>
2021-11-09 01:27:57 -0500

    Fixed the og:image location

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ export default class MainDocument extends Document {
                     <meta property='og:site_name' content="Particle Studios" />
                     <meta property='og:url' content='https://particlestudios.org/' />
                     <meta property='og:description' content='Official website for Particle Studios. Learn more about us and what we do.' />
-                    <meta property='og:image' content='https://particlestudios.org/images/logo.svg' />
+                    <meta property='og:image' content='https://particlestudios.org/logo.svg' />
                     <meta property='og:keywords' content='Particle Studios, Studios, Games, Game Development, C++, Unity, Java, OpenGL, Vulkan' />
                     <link rel="icon" href="/images/logo.svg" />
                 </Head>
