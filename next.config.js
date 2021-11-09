276d4715146c2674a37e0c9182dabf7146ed4bc9
abused_master <techperson71@gmail.com>
2021-11-08 21:41:21 -0500

    Added https prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd ? 'particlestudios.github.io/' : ''
+    assetPrefix: isProd ? 'https://particlestudios.github.io/' : ''
 }
