019d042b17d095e0e5777926eabc7f5b378f82fd
abused_master <techperson71@gmail.com>
2021-11-08 23:59:54 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    assetPrefix: isProd ? 'https://particlestudios.org/particlestudios.github.io/' : ''
+    assetPrefix: isProd ? 'https://particlestudios.org/' : ''
 }
